fe4a0f41f63a1cce6b71f69e23dc51c746f5203b
Luan <97277916+hluan-dev@users.noreply.github.com>
2023-07-15 16:15:25 -0700

    init tauri

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="">
+      <div>This is kinda cool</div>
+      <p>Wow this is pretty cool</p>
+      <p>I wonder this could be the greatest idea ever</p>
+    </main>
+  );
+}
