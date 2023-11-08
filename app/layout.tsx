00b7d9d6215b2c6471dc50bc990a4c5942b913d2
Luan <97277916+hluandev@users.noreply.github.com>
2023-11-07 18:12:22 -0800

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Supertime",
-  description: "Manage your organizations in an easy way",
+  title: "The Japanese Project",
+  description: "The only place to learn Japanese",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main className="">
-      <h1 className="font-bold text-7xl">Supertime</h1>
-      <p className="text-xl">Manage your organizations in an easy way</p>
-    </main>
-  );
+  return <main></main>;
 }
