7ea7dbfd9a4659fd7ec9a32a0aa6336edc54379d
hluandev <97277916+hluandev@users.noreply.github.com>
2024-04-01 00:42:26 -0700

    updated

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="text-green-400">
+      <h1>This is kinda nice</h1>
+      <p>This is kinda cool</p>
+    </main>
+  );
+}
--- main.py
+++
@@ -1,2 +0,0 @@
-print("hello")
-print("hello you are so nice")
