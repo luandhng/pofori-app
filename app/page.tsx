2d2c2fea87faa02afd61c38c580ca98dd0ab6490
Luan <97277916+hluandev@users.noreply.github.com>
2024-04-20 16:08:58 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
+import { supabase } from "@/utils/supabase/client";
+
+const getAdmin = async () => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user;
+};
+
 export default async function Home() {
+  const admin = getAdmin();
+
+  if (!admin) {
+    console.log("none");
+  }
+
   return (
     <main className="h-full flex items-center justify-center">
       <form action="" className="flex flex-col gap-1">
