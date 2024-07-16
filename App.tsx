63a4d5c6d906583bd6406e27621e23498db167d9
ddddddsssssggggg <144175324+ddddddsssssggggg@users.noreply.github.com>
2024-07-16 15:14:53 -0700

    update

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="bg-red-500 h-full flex justify-center items-center">
+    <View className="bg-red-600 h-full flex justify-center items-center">
       <Text className="text-xl text-white font-medium">
-        Please arrive at the location
+        You are not in the zone
       </Text>
 
       {/* <Workers /> */}
